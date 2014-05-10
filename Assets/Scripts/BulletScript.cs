c795cecaa5432db0a2578fa0225dd095badb6df9
Paximillian <morzil@walla.com>
2014-05-10 19:11:43 +0300

    MASTER
    Now you commit to me instead.

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.collider.gameObject.layer == LayerMask.NameToLayer("BulletLimits")){
+			gameObject.SetActive(false);
+			transform.position = new Vector3(50, 13, 69);
+			rigidbody.velocity = Vector3.zero;
+		}
+	}
+}
